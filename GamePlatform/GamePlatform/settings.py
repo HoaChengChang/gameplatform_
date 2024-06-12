c29db6a089c2a6539e01cd36bd5fbd7a6945c3cc
HoaChengJhang <93926929+HoaChengChang@users.noreply.github.com>
2024-06-12 09:29:43 +0800

    Update settings.py

--- GamePlatform/GamePlatform/settings.py
+++ GamePlatform/GamePlatform/settings.py
@@ -195,23 +195,16 @@ CELERY_RESULT_BACKEND = "django-db"
 CELERY_ACCEPT_CONTENT = ['application/json', ]
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-
-# 为任务设置超时时间，单位秒。超时即中止，执行下个任务。
+# 當任務設置超時，單位秒。超時即中止，執行下个任务。
 CELERY_TASK_TIME_LIMIT = 5
-
-# 为存储结果设置过期日期，默认1天过期。如果beat开启，Celery每天会自动清除。
-# 设为0，存储结果永不过期
+# 默认1天過期。如果使用beat，Celery每天会自動清除。
 CELERY_RESULT_EXPIRES = 1
-
-# 任务限流
+# 任務限流
 CELERY_TASK_ANNOTATIONS = {'tasks.work_chain': {'rate_limit': '10/s'}}
-
-# Worker并发数量，一般默认CPU核数，可以不设置
+# Worker併發數量，一般默認CPU核心數，可以不設置
 CELERY_WORKER_CONCURRENCY = 2
-
-# 每个worker执行了多少任务就会死掉，默认是无限的
+# 每个worker執行了多少任務就會終止，默認是無限
 CELERY_WORKER_MAX_TASKS_PER_CHILD = 200
-
 CELERY_BEAT_SCHEDULE = {
     'start-every-week-morning': {
         'task': 'gameApp.tasks.work_chain',
